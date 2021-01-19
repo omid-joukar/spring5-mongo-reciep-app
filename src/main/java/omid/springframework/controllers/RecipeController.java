45bd8ba1a6351bec1269eef5967f5436bdb99684
omid-joukar <joukaromid6@gmail.com>
2021-01-19 10:09:45 +0100

    added exception handler in WebFlux.

--- src/main/java/omid/springframework/controllers/RecipeController.java
+++ src/main/java/omid/springframework/controllers/RecipeController.java
@@ -1,13 +1,16 @@
 package omid.springframework.controllers;
 
 import omid.springframework.commands.RecipeCommand;
+import omid.springframework.exceptions.NotFoundException;
 import omid.springframework.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
+import org.thymeleaf.exceptions.TemplateInputException;
 
 
 /**
@@ -76,20 +79,16 @@ public class RecipeController {
         return "redirect:/";
     }
 
-//    @ResponseStatus(HttpStatus.NOT_FOUND)
-//    @ExceptionHandler(NotFoundException.class)
-//    public ModelAndView handleNotFound(Exception exception){
-//
-//        log.error("Handling not found exception");
-//        log.error(exception.getMessage());
-//
-//        ModelAndView modelAndView = new ModelAndView();
-//
-//        modelAndView.setViewName("404error");
-//        modelAndView.addObject("exception", exception);
-//
-//        return modelAndView;
-//    }
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler({NotFoundException.class, TemplateInputException.class})
+    public String handleNotFound(Model model,Exception exception){
+
+        log.error("Handling not found exception");
+        log.error(exception.getMessage());
+
+        model.addAttribute("exception", exception);
+        return "404error";
+    }
 
 }
 
